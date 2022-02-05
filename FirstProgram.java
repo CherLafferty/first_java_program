29a0fd2a94acce8172412ec7656850e3115e10cf
Cher Lafferty <cherlafferty01@gmail.com>
2022-02-05 14:31:38 -0800

    initial

---
+++ FirstProgram.java
@@ -0,0 +1,7 @@
+public class FirstProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Cher Lafferty");
+        System.out.println("I am 27 years old");
+        System.out.println("I live on Whidbey Island, WA");
+    }
+}
